509da556252052c200c18f94ace5b86b7d748b12
XTRM_LG\USER <saturn5903@gmailcom>
2018-02-02 16:17:17 +0900

    상세보기 기능 구현

--- src/main/java/net/slipp/web/QuestionController.java
+++ src/main/java/net/slipp/web/QuestionController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -51,4 +53,13 @@ public class QuestionController {
 		return "redirect:/";
 	}
 	
+	
+	@GetMapping("/{id}")
+	public String show(@PathVariable Long id, Model model) {
+		//Question question = questionRepository.findOne(id); 
+		//model.addAttribute("question", question);
+		
+		model.addAttribute("question", questionRepository.findOne(id) );
+		return "/qna/show";
+	}
 }
