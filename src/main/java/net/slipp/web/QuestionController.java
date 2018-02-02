3600104cb8c80f0e9ee7e85a83b799ef836b4d66
XTRM_LG\USER <saturn5903@gmailcom>
2018-02-02 17:19:23 +0900

    삭제 기능 구현 완료

--- src/main/java/net/slipp/web/QuestionController.java
+++ src/main/java/net/slipp/web/QuestionController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -80,6 +81,12 @@ public class QuestionController {
 		return String.format("redirect:/questions/%d", id);
 	}
 	
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable Long id){
+		questionRepository.delete(id);
+		return "redirect:/";
+		
+	}
 	
 	
 }
